1d83e26c077a2c774276a61695816d8b993faa4d
tarahform <ptarahform@gmail.com>
2018-05-21 16:16:43 -0500

    part one done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -55,7 +55,7 @@ function start() {
                         }
                         console.log("Purchase Successful.");
                         console.log("Total Purchase: " + total);
-                        start()
+                        start();
                     });
                 }
             });
